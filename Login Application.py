1416a28c0eb67290ce47366d5189e3880470723d
Vishnu Babalsure <vishnubabalsure114@yahoo.com>
2025-10-05 22:28:05 +0530

    Login Application.py

---
+++ Login Application.py
@@ -0,0 +1,32 @@
+print("Admin login".center(50)) 
+
+
+username=input("enter username:")
+password=input("enter password:")
+
+corret_username="thorat"
+corret_password="thorat123"
+if username==corret_username and password==corret_password:
+    print("successfully enter".center(50))
+    print("Admin Data:")
+    print("Name = Nilesh Chatap")
+    print("Email ID = nileshchatap25@gmail.com")
+    
+    
+else:
+    
+    
+    print("Not inter,please registiation".center(65))
+    
+    
+    name1=input("Enter Name:")
+    
+    name2=input("Enter DOB:")
+    
+    name3=input("Enter Email:")
+   
+    name4=input("M.number:")
+  
+    name5=input("Highest Qualification:")
+    
+    print("Successfully Registiation".center(50))
